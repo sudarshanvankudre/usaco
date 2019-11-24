5123307320652e86a551c488972a32f31c824db3
sudarshanvankudre <joecurious7@gmail.com>
2019-11-23 23:18:39 -0800

    Made some more changes to pass more tests.

--- Broken Necklace/beads.py
+++ Broken Necklace/beads.py
@@ -4,34 +4,44 @@ LANG: PYTHON3
 TASK: beads
 """
 def beads_right(necklace):
-    count = 1
-    prev = len(necklace) - 1
-    for i in range(len(necklace) - 2, -1, -1):
-        if necklace[i] == necklace[prev] or necklace[prev] == 'w':
+    count = 0
+    for i in range(len(necklace) - 1, -1, -1):
+        if necklace[i] != 'w':
+            color = necklace[i]
+            break
+        if i == 0:
+            color = 'w'
+    # print(color)
+    for i in range(len(necklace) - 1, -1, -1):
+        if necklace[i] == color or necklace[i] == 'w':
             count += 1
-            prev -= 1
         else:
             break
     return count
 
 def beads_left(necklace):
-    count = 1
-    prev = 0
-    for i in range(1, len(necklace)):
-        if necklace[i] == necklace[prev] or necklace[prev] == 'w':
+    count = 0
+    for i in range(len(necklace)):
+        if necklace[i] != 'w':
+            color = necklace[i]
+            break
+        if i == len(necklace) - 1:
+            color = 'w'
+    # print(color)
+    for i in range(len(necklace)):
+        if necklace[i] == color or necklace[i] == 'w':
             count += 1
-            prev += 1
         else:
             break
     return count
 
 def beads_collected(cut, necklace):
     if cut == 0:
-        print(cut, necklace)
+        # print(cut, necklace)
         return beads_right(necklace) + beads_left(necklace)
     else:
         cut_necklace = necklace[cut:] + necklace[:cut]
-        print(cut, cut_necklace)
+        # print(cut, cut_necklace)
         return beads_right(cut_necklace) + beads_left(cut_necklace)
 
 with open("beads.in", "r") as fin:
@@ -40,7 +50,10 @@ with open("beads.in", "r") as fin:
 max_beads = 0
 for i in range(N):
     collected = beads_collected(i, necklace)
-    print(i, collected)
+    # print(i, collected)
     if collected > max_beads:
         max_beads = collected
-print(max_beads)
+if max_beads == 2 * N:
+    max_beads = N
+with open("beads.out", 'w') as fout:
+    fout.write(str(max_beads) + "\n")
