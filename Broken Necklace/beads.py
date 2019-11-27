ebc58feda2bb1d5991fabe63098894fa648dd25a
sudarshanvankudre <joecurious7@gmail.com>
2019-11-27 12:18:00 -0800

    Solved.

--- Broken Necklace/beads.py
+++ Broken Necklace/beads.py
@@ -3,57 +3,69 @@ ID: sudarsh11
 LANG: PYTHON3
 TASK: beads
 """
-def beads_right(necklace):
-    count = 0
-    for i in range(len(necklace) - 1, -1, -1):
-        if necklace[i] != 'w':
-            color = necklace[i]
-            break
-        if i == 0:
-            color = 'w'
-    # print(color)
-    for i in range(len(necklace) - 1, -1, -1):
-        if necklace[i] == color or necklace[i] == 'w':
-            count += 1
-        else:
-            break
-    return count
+def is_uniform(necklace):
+    """
+    >>> is_uniform('bbbbbb')
+    True
+    >>> is_uniform('bbbwwbw')
+    True
+    >>> is_uniform('bbbrbbb')
+    False
+    """
+    color = None
+    for b in necklace:
+        if not color and b != 'w':
+            color = b
+        elif b != color and b != 'w':
+            return False
+    return True
 
-def beads_left(necklace):
-    count = 0
-    for i in range(len(necklace)):
-        if necklace[i] != 'w':
-            color = necklace[i]
+def count(necklace):
+    """
+    >>> count('bwbrbbr')
+    3
+    >>> count('bbbrrr')
+    3
+    >>> count('wwwrwb')
+    5
+    >>> count('rrwwwwbb')
+    6
+    """
+    beads = 0
+    color = None
+    stop_index = -1
+    for i, b in enumerate(necklace):
+        if not color and b != 'w':
+            color = b
+        elif b != color and b != 'w':
+            stop_index = i
             break
-        if i == len(necklace) - 1:
-            color = 'w'
-    # print(color)
-    for i in range(len(necklace)):
-        if necklace[i] == color or necklace[i] == 'w':
-            count += 1
-        else:
-            break
-    return count
+        beads += 1
+    return beads, stop_index
 
-def beads_collected(cut, necklace):
-    if cut == 0:
-        # print(cut, necklace)
-        return beads_right(necklace) + beads_left(necklace)
-    else:
-        cut_necklace = necklace[cut:] + necklace[:cut]
-        # print(cut, cut_necklace)
-        return beads_right(cut_necklace) + beads_left(cut_necklace)
-
-with open("beads.in", "r") as fin:
+with open("beads.in", 'r') as fin:
     N = int(fin.readline())
     necklace = fin.readline().rstrip()
+
 max_beads = 0
-for i in range(N):
-    collected = beads_collected(i, necklace)
-    # print(i, collected)
-    if collected > max_beads:
-        max_beads = collected
-if max_beads == 2 * N:
+if is_uniform(necklace):
+    print("necklace is uniform")
     max_beads = N
+else:
+    for i in range(N):
+        new_necklace = necklace[i:] + necklace[:i]
+        beads1, stop_index = count(new_necklace)
+        stop_index = N - stop_index
+        new_necklace = new_necklace[::-1][:stop_index]
+        beads2 = 0
+        color = None
+        for b in new_necklace:
+            if not color and b != 'w':
+                color = b
+            elif b != color and b != 'w':
+                break
+            beads2 += 1
+        beads = beads1 + beads2
+        max_beads = max(max_beads, beads)
 with open("beads.out", 'w') as fout:
     fout.write(str(max_beads) + "\n")
