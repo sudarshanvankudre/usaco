a44242ea311685c71cc52f94afb53dfcb0649129
sudarshanvankudre <joecurious7@gmail.com>
2019-12-17 00:07:24 -0800

    Have something that works for max milking time.

--- Milking Cows/milk2.py
+++ Milking Cows/milk2.py
@@ -3,24 +3,17 @@ ID: sudarsh11
 LANG: PYTHON3
 TASK: milk2
 """
-
-class Interval:
-    def __init__(self, s, e):
-        assert s <= e, "The start must be <= the end"
-        self.start = s
-        self.end = e
-
-    def contained_in(self, interval):
-        return self.start >= interval.start and self.end <= interval.end
-
-    def intersects_above(self, interval):
-        return self.start <= interval.end
-
-    def intersects_below(self, interval):
-        return self.end >= interval.start
-
 with open("milk2.in", 'r') as fin:
     N = int(fin.readline())
-    intervals = set()
-    for _ in range(N):
-    	
+    milking_intervals = []
+    for line in map(str.rstrip, fin):
+        interval = tuple(map(int, line.split()))
+        milking_intervals.append(interval)
+
+max_milking_time = 0
+for i in range(N):
+    max_milking_time = max(max_milking_time, milking_intervals[i][1] - milking_intervals[i][0])
+    for j in range(N):
+        if i != j:
+            if milking_intervals[i][0] <= milking_intervals[j][0] <= milking_intervals[i][1]:
+                max_milking_time = max(max_milking_time, milking_intervals[j][1] - milking_intervals[i][0])
