cac1101dec43f71dec51c055be45143f528976f6
sudarshanvankudre <joecurious7@gmail.com>
2019-12-17 15:14:00 -0800

    Solved.

--- Milking Cows/milk2.py
+++ Milking Cows/milk2.py
@@ -3,51 +3,36 @@ ID: sudarsh11
 LANG: PYTHON3
 TASK: milk2
 """
-def interval(start, end):
-    return start, end
-
-def start(interval):
-    return interval[0]
-
-def end(interval):
-    return interval[1]
-
-def merge(a, b):
-    """Returns a merged interval of overlapping intervals"""
-    if start(a) <= start(b) <= end(a) <= end(b):
-        return start(a), end(b)
-    elif start(b) <= start(a) <= end(b) <= end(a):
-        return start(b), end(a)
-
-def overlap(a, b):
-    """Returns true if a and b overlap"""
-    return start(a) <= start(b) <= end(a) <= end(b) or start(b) <= start(a) <= end(b) <= end(a)
-
-def inside(a, b):
-    """True if a is within b. ie, start(b) <= start(a) < end(a) <= end(b)"""
-    return start(b) <= start(a) <= end(a) <= end(b)
-
-def merge_list(lst):
-    """Merges all intervals in lst and returns the resulting list"""
-
+import heapq
 
 with open("milk2.in", 'r') as fin:
     N = int(fin.readline())
     milking_intervals = []
     for line in map(str.rstrip, fin):
         temp = tuple(map(int, line.split()))
-        milking_intervals.append(interval(temp[0], temp[1]))
+        heapq.heappush(milking_intervals, (temp[0], temp[1]))
 
+mint = milking_intervals[:]
+prev = list(heapq.heappop(mint))
+max_milking_time = prev[1] - prev[0]
+while len(mint) > 0:
+    temp = heapq.heappop(mint)
+    if prev[0] <= temp[0] <= prev[1] <= temp[1]:
+        max_milking_time = max(max_milking_time, temp[1] - prev[0])
+        prev[1] = temp[1]
+    elif temp[0] > prev[1]:
+        max_milking_time = max(max_milking_time, temp[1] - temp[0])
+        prev = list(temp)
 
-max_milking_time = 0
-for i in range(N):
-    max_milking_time = max(max_milking_time, milking_intervals[i][1] - milking_intervals[i][0])
-    for j in range(N):
-        if i != j:
-            if milking_intervals[i][0] <= milking_intervals[j][0] <= milking_intervals[i][1]:
-                max_milking_time = max(max_milking_time, milking_intervals[j][1] - milking_intervals[i][0])
+max_idle_time = 0
+t = heapq.heappop(milking_intervals)
+curr = t[1]
+while len(milking_intervals) > 0:
+    temp = heapq.heappop(milking_intervals)
+    if curr < temp[0]:
+        max_idle_time = max(max_idle_time, temp[0] - curr)
+    if curr < temp[1]:
+        curr = temp[1]
 
-class Interval:
-    def __init__(self, start, end):
-        self.start = start
-        self.end = end
+with open("milk2.out", 'w') as fout:
+    fout.write("{} {}\n".format(str(max_milking_time), str(max_idle_time)))
