3c4ee2121925e02e4c4052f7027f47b89e894222
sudarshanvankudre <joecurious7@gmail.com>
2019-12-17 18:40:44 -0800

    Got inputs in.

--- Transformations/transform.py
+++ Transformations/transform.py
@@ -3,3 +3,14 @@ ID: sudarsh11
 LANG: PYTHON3
 TASK: transform
 """
+
+def ninety_rotation(pattern):
+    """Rotates and returns pattern, which is an NxN matrix by 90 degrees clockwise.
+    >>> ninety_rotation([['@', '-', '@'], ['-', '-', '-'], ['@', '@', '-']])
+    [['@', '-', '@'], ['@', '-', '-'], ['-', '-', '@']]
+    """
+
+with open("transform.in", 'r') as fin:
+    N = int(fin.readline())
+    og = [list(fin.readline().rstrip()) for _ in range(N)]
+    trans = [list(fin.readline().rstrip()) for _ in range(N)]
