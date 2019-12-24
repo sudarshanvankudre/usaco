bdb2cde154c495c3d68727f50c6b4d39719745d3
Vankudre <joecurious7@gmail.com>
2019-12-24 10:45:16 -0800

    Almost working.

--- Mixing Milk/milk.py
+++ Mixing Milk/milk.py
@@ -8,6 +8,18 @@ import heapq
 market = []
 with open("milk.in", "r") as fin:
     N, M = map(int, fin.readline().rstrip().split())
-    for line in map(lambda x: x.rstrip().split(), fin):
-        price, amount = map(int, list(line))
-        print(price, amount)
+    for line in map(str.rstrip, fin):
+        price, amount = map(int, line.split())
+        heapq.heappush(market, (price, amount))
+
+min_cost = 0
+while N > 0:
+    price, amount = heapq.heappop(market)
+    if amount > N:
+        min_cost += price * (amount - N)
+        N -= amount - N
+    else:
+        min_cost += price * amount
+        N -= amount
+with open("milk.out", "w") as fout:
+    fout.write(str(min_cost) + "\n")
