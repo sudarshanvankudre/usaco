0edbb2de22db477fc3f7ecb6621f37c454845d7b
Vankudre <joecurious7@gmail.com>
2019-12-23 22:44:44 -0800

    Some initial progress.

--- Mixing Milk/milk.py
+++ Mixing Milk/milk.py
@@ -5,5 +5,9 @@ TASK: milk
 """
 import heapq
 
+market = []
 with open("milk.in", "r") as fin:
-    
+    N, M = map(int, fin.readline().rstrip().split())
+    for line in map(lambda x: x.rstrip().split(), fin):
+        price, amount = map(int, list(line))
+        print(price, amount)
