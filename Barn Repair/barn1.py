ab3809321e659468e296513e5e9c2ebdb43387ec
Vankudre <joecurious7@gmail.com>
2019-12-24 22:32:48 -0800

    Some strategy realizations.

--- Barn Repair/barn1.py
+++ Barn Repair/barn1.py
@@ -3,3 +3,9 @@ ID: sudarsh11
 LANG: PYTHON3
 TASK: barn1
 """
+with open("barn1.in", "r") as fin:
+    M, S, C = map(int, fin.readline().rstrip().split())
+# realization: the more 'granular' the number of boards, the less wasted space.
+# realization: we must use all of the available boards
+# conclusion: we have to find the least wasteful partition of the stalls using
+# the max number of boards possible
