303a4063ea3b5521780e1f9b1001512b52dc108b
Vankudre <joecurious7@gmail.com>
2020-01-19 14:35:03 -0800

    Slow progress.

--- Barn Repair/barn1.py
+++ Barn Repair/barn1.py
@@ -14,14 +14,17 @@ def end(interval):
 
 def size(interval):
     return interval[1] - interval[0] + 1
-    
+
 with open("barn1.in", "r") as fin:
     M, S, C = map(int, fin.readline().rstrip().split())
-    full_stalls = sorted(stall for stall in map(int, fin))
+    cow_stalls = sorted(stall for stall in map(int, fin))
 
 
 # we have M boards which we can use. The greedy solution is to find the best
 # solution for some amount of boards and then use that. we want to iterate
 # through every possible number of boards
-stalls_blocked = full_stalls[len(full_stalls) - 1] - full_stalls[0] + 1
-for possible_boards in range(M):
+num_stalls_blocked = cow_stalls[len(full_stalls) - 1] - cow_stalls[0] + 1
+covered_stalls = [interval(cow_stalls[0], cow_stalls[1])]
+for possible_boards in range(2, M + 1):
+        for _ in range(possible_boards):
+            
