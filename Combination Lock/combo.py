bdf5a12af049b1d55b0675000a058e935fbb4584
sudarshan <joecurious7@gmail.com>
2020-04-28 21:03:35 -0700

    Finished brute force strategy, but too slow.

--- Combination Lock/combo.py
+++ Combination Lock/combo.py
@@ -1,25 +1,42 @@
 """
 ID: sudarsh11
 LANG: PYTHON3
-TASK: crypt1
+TASK: combo
 """
+import itertools
 
 with open('combo.in', 'r') as fin:
     N = int(fin.readline())
     john_combo = [int(i) for i in fin.readline().split()]
     master_combo = [int(i) for i in fin.readline().split()]
 
-def min_tolerance(n):
-    return ((n - 2) % N) + 1
+def tolerance(x):
+    """Returns a set of numbers that are "close" to x for combination purposes"""
+    ans = set()
+    for i in range(-2, 3):
+        ans.add((x - 1 + i) % N + 1)
+    return ans
 
-def max_tolerance(n):
-    return ((n + 2) % N) + 1
+def john_valid(combo):
+    """Returns true if combo is valid for john_combo"""
+    for j, i in zip(john_combo, combo):
+        # print("j: ", j)
+        # print("i: ", i)
+        if i not in tolerance(j):
+            return False
+    return True
 
-range1 = 0
-range2 = 0
-range3 = 0
+def master_valid(combo):
+    """Returns true if combo is valid for master_combo"""
+    for m, i in zip(master_combo, combo):
+        if i not in tolerance(m):
+            return False
+    return True
 
-for i in range(3):
-    n1 = john_combo[i]
-    n2 = master_combo[i]
-    
+count = 0
+for c in itertools.product(range(1, N + 1), repeat=3):
+    if john_valid(c) or master_valid(c):
+        count += 1
+
+with open('combo.out', 'w') as fout:
+    fout.write(str(count) + "\n")
